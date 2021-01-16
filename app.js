72fbd85f6c592a3f48ce94f4ec1874cc190e913a
Vitaliy <vetalpavlishin@gmail.com>
2021-01-16 03:27:59 +0200

    update mongoose connect

--- app.js
+++ app.js
@@ -22,7 +22,10 @@ app. use(cors());
 
 require('dotenv').config({path:'.env'});
 
-mongoose.connect (process.env.MONGO_URI);
+mongoose.connect (process.env.MONGO_URI, 
+  {
+  useNewUrlParser: true
+});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind (console, 'connection error:'));
